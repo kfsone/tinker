24ce0796c1b8871cd4a6255c5303667a0d9b4092
Oliver Smith <oliver@kfs.org>
2018-09-28 18:28:51 -0700

    Working, but slow

--- python/sync/sftpsession.py
+++ python/sync/sftpsession.py
@@ -1,33 +1,67 @@
 import logging
 from paramiko import client as pmclient
 
+import time
 
-class SFTPSession(object):
-    def __init__(self, hostname, username, password, initial_path=None, logger=None):
-        self.__ssh_client = ssh = pmclient.SSHClient()
-        ssh.set_missing_host_key_policy(pmclient.AutoAddPolicy())
+
+class SSHSession(object):
+    def __init__(self, hostname, username, password, logger=None):
         self.logger = logger or logging
 
-        logger.debug("Connecting to %s with %s and password", hostname, username)
+        self.client = ssh = pmclient.SSHClient()
+        ssh.set_missing_host_key_policy(pmclient.AutoAddPolicy())
+
+        self.logger.debug("SSHClient: Connecting to %s with %s and password", hostname, username)
         ssh.connect(hostname, username=username, password=password, look_for_keys=False)
 
-        logger.debug("opening sftp session")
-        self.client = sftp = ssh.open_sftp()
 
-        if initial_path:
-            logger.debug("remote cd: %s", initial_path)
-            sftp.chdir(initial_path)
+    def execute(self, command, inputs=None, read_stdout=False, *args, **kwargs):
+        stdin, stdout, stderr = self.client.exec_command(command, *args, **kwargs)
+
+        if inputs:
+            stdin.channel.sendall(inputs)
+            stdin.channel.shutdown_write()
+
+        while True:
+            if stdout.channel.exit_status_ready():
+                break
+            time.sleep(0.001)
+
+        rc = stdout.channel.recv_exit_status()
+        if read_stdout:
+            return rc, stdout.read()
+        else:
+            return rc
 
 
     def close(self):
         if self.client:
-            self.logger.debug("Shutting down sftp session")
+            self.logger.debug("Shutting down SSH session")
             self.client.close()
-            del self.client
+            self.client = None
 
-        if self.__ssh_client:
-            self.logger.debug("Shutting down ssh session")
+
+class SFTPSession(object):
+    def __init__(self, hostname=None, username=None, password=None, initial_path=None, logger=None, ssh=None):
+        assert ssh or (hostname and username and password)
+        self.__own_ssh = not bool(ssh)
+        ssh = self.__ssh_client = ssh or SSHSession(hostname, username, password, logger=logger)
+        self.logger = logger or logging
+        logger.debug("SFTP: Opening session")
+        self.client = sftp = ssh.client.open_sftp()
+
+        if initial_path:
+            self.logger.debug("remote cd: %s", initial_path)
+            sftp.chdir(initial_path)
+
+    def close(self):
+        if self.client:
+            self.logger.debug("Shutting down SFTP session")
+            self.client.close()
+            self.client = None
+
+        if self.__own_ssh and self.__ssh_client:
             self.__ssh_client.close()
-            del self.__ssh_client
+            self._ssh_client = None
 
 
